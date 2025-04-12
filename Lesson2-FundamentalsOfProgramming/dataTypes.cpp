6c4acd25d33c431a1930731fb7b340397db10335
Rudraksh <rudraksh05jain@gmail.com>
2025-04-12 12:29:49 +0530

    first commit

---
+++ Lesson2-FundamentalsOfProgramming/dataTypes.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // cout<<"hello world!";
+    int firstNumber = -52;
+    float firstFloatingNumber = 52.034;
+    char character = 'a';
+    bool boolean = true; //false
+    cout<<"firstNumber : "<<firstNumber<<endl;
+    cout<<"firstFloatingNumber : "<<firstFloatingNumber<<endl;
+    cout<<"character : "<<character<<endl;
+    cout<<"boolean : "<<int(boolean)<<endl;
+    return 0;
+}
