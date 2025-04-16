3e581f0c29aa1247679f9dd7f27cbb6544d72a51
r-afool <rudraksh05jain@gmail.com>
2025-04-16 12:27:50 +0530

    Lesson 5

---
+++ Lesson5/clapGame.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 1; i <= n; i++){
+        if(i%5==0){
+            cout<<"clap"<<endl;
+            continue;
+        }
+        cout<<i<<endl;
+    }
+}
---
+++ Lesson5/operatorJargon.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    cout<<a++<<a++<<a<<++a;
+    //656
+    //667
+    //676
+}
