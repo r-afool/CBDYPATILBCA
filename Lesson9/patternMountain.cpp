ce1d3c371dacacdfc159ba0268c33daa3e8f1e53
r-afool <rudraksh05jain@gmail.com>
2025-04-22 12:41:49 +0530

    Lesson 9

---
+++ Lesson9/patternMountain.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout<<j<<'\t';
+        }
+        for(int j = 0; j < n-i; j++){
+            cout<<" \t";
+        }
+        for(int j = 0; j < n-i-1; j++){
+            cout<<" \t";
+        }
+        for(int j = i; j >= 1; j--){
+            if(j==n){
+                continue;
+            }
+            cout<<j<<"\t";
+        }
+        cout<<endl;
+    }
+}
